7b4a1d8cdf4c5dad457fc242a825d771a6b18873
jaider <jaider.grico>
2025-03-07 17:13:19 -0500

    subida de clase carro

--- carro.cpp
+++ carro.cpp
@@ -1,8 +1,47 @@
-#include <stdio.h>
+#include "./Carro.h"
 
-int main()
-{
-    printf("Hello World");
-
-    return 0;
+void Carro::Informacion(){
+    
+    cout << "Kilometraje: " << kilometraje << endl;
+    cout << "Nivel de gasolina: " << nivelGasolina << " litros" << endl;
+    
 }
+
+Carro::Carro(){
+    
+    kilometraje = 0;
+    nivelGasolina = 0;
+    estado = '0';
+    
+}
+
+void Carro::Recorrer(float distancia){
+    
+    float distanciaReal = min(nivelGasolina*autonomia, distancia);
+    
+    kilometraje = kilometraje + distanciaReal;
+    nivelGasolina = nivelGasolina - (distanciaReal/autonomia);
+    
+}
+
+float Carro::Tanquear(float gasolina){
+    
+    
+    float faltante = capacidad - nivelGasolina;
+    float sobrante = gasolina - faltante;
+    
+    if(sobrante >= 0){
+        
+        nivelGasolina = capacidad;
+        return sobrante;
+        
+    }else{
+        
+        nivelGasolina = nivelGasolina + gasolina; 
+        return 0;
+        
+    }
+    
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "./Tecnico.h"
+
+int main(){
+    
+    Carro goku;
+    Tecnico vegeta;
+    
+    Carro* ptr = &goku;
+    
+    vegeta.Tanquear(ptr,20);
+    
+    goku.Informacion();
+    
+    //cout << "sobrante: " << sobrante << endl;
+    
+    AutoServicio maquina;
+    
+    maquina.ListarPrecios();
+    
+    return 0;
+}
+
